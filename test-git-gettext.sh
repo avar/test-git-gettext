9e242f087cdf1545492c5e32cb0db361bbcebe51
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-06-14 09:25:54 -0700

    verbose and non-verbose output

--- test-git-gettext.sh
+++ test-git-gettext.sh
@@ -17,13 +17,18 @@ uname -a > $LOG
 git clean -dxf > /dev/null
 $MAKE prefix=/tmp all NO_GETTEXT=YesPlease
 echo "With NO_GETTEXT=YesPlease" >> $LOG
-(cd t && for test in ./t02*sh; do ./$test -d -v; done) >> $LOG 2>&1
+echo "With NO_GETTEXT=YesPlease" >> "$LOG-raw"
+(cd t && for test in ./t02*sh; do ./$test; done) >> $LOG 2>&1
+(cd t && for test in ./t02*sh; do ./$test -d -v; done) >> "$LOG-raw" 2>&1
 
 git clean -dxf > /dev/null
 $MAKE prefix=/tmp all
 echo "Without NO_GETTEXT=YesPlease" >> $LOG
-(cd t && for test in ./t02*sh; do ./$test -d -v; done) >> $LOG 2>&1
+echo "Without NO_GETTEXT=YesPlease" >> "$LOG-raw"
+(cd t && for test in ./t02*sh; do ./$test; done) >> $LOG 2>&1
+(cd t && for test in ./t02*sh; do ./$test -d -v; done) >> "$LOG-raw" 2>&1
 
 # Gimme test data
-cat $LOG
+cat "$LOG-raw" >> $LOG
+rm "$LOG-raw"
 echo "Feed $LOG to a pastebin please"
