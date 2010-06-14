b4baa4d6addf50565db8456d68094744ec20337e
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-06-14 22:12:11 +0000

    Reverting "Use bash, not ./, solaris can't handle $() in /bin/sh"

--- test-git-gettext.sh
+++ test-git-gettext.sh
@@ -18,20 +18,20 @@ git clean -dxf > /dev/null
 $MAKE -j4 prefix=/tmp all NO_CURL=YesPlease NO_GETTEXT=YesPlease
 echo "With NO_GETTEXT=YesPlease" >> $LOG
 echo "With NO_GETTEXT=YesPlease" >> "$LOG-raw"
-(cd t && for test in ./t02*sh; do bash ./$test; done) | sed 's/^/    /' >> $LOG 2>&1
-(cd t && for test in ./t02*sh; do bash ./$test -d -v; done) | sed 's/^/    /' >> "$LOG-raw" 2>&1
+(cd t && for test in ./t02*sh; do ./$test; done) | sed 's/^/    /' >> $LOG 2>&1
+(cd t && for test in ./t02*sh; do ./$test -d -v; done) | sed 's/^/    /' >> "$LOG-raw" 2>&1
 
 git clean -dxf > /dev/null
 $MAKE -j4 prefix=/tmp all NO_CURL=YesPlease
 echo "Without NO_GETTEXT=YesPlease" >> $LOG
 echo "Without NO_GETTEXT=YesPlease" >> "$LOG-raw"
-(cd t && for test in ./t02*sh; do bash ./$test; done) | sed 's/^/    /' >> $LOG 2>&1
-(cd t && for test in ./t02*sh; do bash ./$test -d -v; done) | sed 's/^/    /' >> "$LOG-raw" 2>&1
+(cd t && for test in ./t02*sh; do ./$test; done) | sed 's/^/    /' >> $LOG 2>&1
+(cd t && for test in ./t02*sh; do ./$test -d -v; done) | sed 's/^/    /' >> "$LOG-raw" 2>&1
 
 echo "Without NO_GETTEXT=YesPlease and TEST_GIT_I18N_EXHAUSTIVE=1 set" >> $LOG
 echo "Without NO_GETTEXT=YesPlease and TEST_GIT_I18N_EXHAUSTIVE=1 set" >> "$LOG-raw"
-(cd t && for test in ./t02*sh; do TEST_GIT_I18N_EXHAUSTIVE=1 bash ./$test; done) | sed 's/^/    /' >> $LOG 2>&1
-(cd t && for test in ./t02*sh; do TEST_GIT_I18N_EXHAUSTIVE=1 bash ./$test -d -v; done) | sed 's/^/    /' >> "$LOG-raw" 2>&1
+(cd t && for test in ./t02*sh; do TEST_GIT_I18N_EXHAUSTIVE=1 ./$test; done) | sed 's/^/    /' >> $LOG 2>&1
+(cd t && for test in ./t02*sh; do TEST_GIT_I18N_EXHAUSTIVE=1 ./$test -d -v; done) | sed 's/^/    /' >> "$LOG-raw" 2>&1
 
 # Gimme test data
 cat "$LOG-raw" >> $LOG
